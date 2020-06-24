05577188b5acd5106d3852b4e3a5aed03b9d20ae
Ashish Panigrahi <panigrahi.ashish1999@gmail.com>
2020-06-24 22:26:45 +0000

    latest fix

--- .local/bin/shutdown.sh
+++ .local/bin/shutdown.sh
@@ -4,4 +4,4 @@
 # './prompt "Do you want to shutdown?" "shutdown -h now"
 
 [ $(echo -e "No\nYes" | dmenu -i -p "Do you want to shutdown?") \
-	== "Yes" ] && shutdown -h now 
+	= "Yes" ] && shutdown -h now 
