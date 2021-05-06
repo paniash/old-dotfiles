cc605d52dca624f892716c81e173f47872447274
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-05-07 00:11:09 +0530

    Updated conf.sh

--- .config/ytfzf/conf.sh
+++ .config/ytfzf/conf.sh
@@ -1,4 +1,5 @@
 YTFZF_HIST=1
 YTFZF_CACHE=~/.cache/ytfzf
 YTFZF_LOOP=1
-YTFZF_PREF="bestvideo[height<=?1080]+bestaudio/best"
+YTFZF_PREF="bestvideo[height<=?720]+bestaudio/best"
+YTFZF_EXTMENU="dmenu -F -i -p 'Search: ' -l 20"
